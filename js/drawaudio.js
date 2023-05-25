f756282d6814c75afedb03a57457dc5347f76401
jyhyun1008 <hyun1008@kasi.re.kr>
2023-05-25 13:22:01 +0900

    가기전 마지막 커밋

--- js/drawaudio.js
+++ js/drawaudio.js
@@ -35,7 +35,7 @@ const drawAudio = url => {
  */
 const filterData = audioBuffer => {
   const rawData = audioBuffer.getChannelData(0); // We only need to work with one channel of data
-  const samples = 140; // Number of samples we want to have in our final data set
+  const samples = 1400; // Number of samples we want to have in our final data set
   const blockSize = Math.floor(rawData.length / samples); // the number of samples in each subdivision
   const filteredData = [];
   for (let i = 0; i < samples; i++) {
