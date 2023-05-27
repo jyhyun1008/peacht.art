61fb1ed5a4d0ba7e837b97aa042034a7b2054bd2
jyhyun1008 <hyun1008@kasi.re.kr>
2023-05-28 05:19:48 +0900

    add some css

--- js/drawaudio.js
+++ js/drawaudio.js
@@ -1,17 +1,3 @@
-/*
-    This is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
 
 // Set up audio context
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -146,7 +132,7 @@ const drawLineSegment = (ctx, x, height, width, isEven) => {
 
 const addAudio = (url, title, index, delay) => {
     audioArray.push(new Audio(url));
-    document.getElementsByClassName('tracklist')[0].innerHTML += '<div class="track_item" >'+title+'<canvas class="track_canvas"></canvas></div>';
+    document.getElementsByClassName('tracklist')[0].innerHTML += '<div class="track_item" ><div class="track_title">'+title+'</div><canvas class="track_canvas"></canvas></div>';
     drawAudio(url, index, delay);
 }
 
