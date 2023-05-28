89f5d4b4fbf398707ca101ce7109e967a49d92f0
jyhyun1008 <hyun1008@kasi.re.kr>
2023-05-29 06:12:08 +0900

    마진 추가

--- js/index.js
+++ js/index.js
@@ -132,7 +132,7 @@ var song = qs.song;
 
 
 const addAudio = (BPM, BEAT, url, title, artist, avatar, index, delay) => {
-    document.getElementsByClassName('tracklist')[0].innerHTML += '<div class="track_item" ><div class="track_controller" style="bottom: '+(trackCounts*113.8 - index*113.8 + 68)+'px;"><span class="track_title">'+title+' by  <img src="'+avatar+'" class="user_avatar">'+artist+'</span><span class="track_mute">M</span></div><canvas class="track_canvas"></canvas></div>';
+    document.getElementsByClassName('tracklist')[0].innerHTML += '<div class="track_item" ><div class="track_controller" style="bottom: '+(trackCounts*113.8 - index*113.8 + 68)+'px;"><span class="track_title"><span>'+title+' by </span><img src="'+avatar+'" class="user_avatar">'+artist+'</span><span class="track_mute">M</span></div><canvas class="track_canvas"></canvas></div>';
     drawAudio(BPM, BEAT, url, index, delay);
 }
 
